613b4ef6aca52ec8c62754e509f8ff1575c58d58
larii <laridudaa1710@gmail.com>
2026-09-17 10:30:19 -0300

    Add files via upload

---
+++ calculadora_simples/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FormCalculadora from './Components/FormCalculadora'
+function App() {
+
+  return (
+    <>
+     <div>
+      <FormCalculadora />
+     </div>
+    </>
+  )
+}
+
+export default App
